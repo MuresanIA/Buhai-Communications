e926342f4f20017b4fff4e96bad177ac2ecec376
MuresanIA <muresan.alexandru933@gmail.com>
2020-08-30 19:02:37 +0300

    first commit

---
+++ src/main/java/com/mia/BuhaiCommunications/BuhaiCommunicationsApplication.java
@@ -0,0 +1,13 @@
+package com.mia.BuhaiCommunications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuhaiCommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuhaiCommunicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mia/BuhaiCommunications/BuhaiCommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mia.BuhaiCommunications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuhaiCommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
