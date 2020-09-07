b384ef51c4714071dda513f02072b407539e8335
MuresanIA <muresan.alexandru933@gmail.com>
2020-09-07 20:45:29 +0300

    created sender repository

---
+++ src/main/java/com/mia/BuhaiCommunications/repository/SenderRepository.java
@@ -0,0 +1,7 @@
+package com.mia.BuhaiCommunications.repository;
+
+import com.mia.BuhaiCommunications.Sender.Sender;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SenderRepository extends JpaRepository<Sender, Integer> {
+}
