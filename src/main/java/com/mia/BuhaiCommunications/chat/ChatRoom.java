6032be78608d10557ca48b83a4c294c24e4fd7d0
MuresanIA <muresan.alexandru933@gmail.com>
2020-09-12 15:02:40 +0300

    Created My Connection Factory Class, Chat Room Rest Controller and added rabbitmq dependency

--- src/main/java/com/mia/BuhaiCommunications/chat/ChatRoom.java
+++ src/main/java/com/mia/BuhaiCommunications/chat/ChatRoom.java
@@ -1,15 +1,14 @@
 package com.mia.BuhaiCommunications.chat;
 
+import com.mia.BuhaiCommunications.Sender.Sender;
+import com.mia.BuhaiCommunications.receiver.Receiver;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
@@ -28,12 +27,18 @@ public class ChatRoom {
             strategy = "uuid2",
             parameters = @Parameter(
                     name = "uuid_gen_strategy_class",
-                    value = "com.mia.BuhaiCommunications.chat.PostgreSQLUUIDGenerationStrategy"
+                    value = "com.vladmihalcea.book.hpjp.hibernate.identifier.uuid.PostgreSQLUUIDGenerationStrategy"
             )
     )
 
     private UUID id;
 
-    private String name;
+    private String chatRoomName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Sender sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Receiver receiver;
 
 }
